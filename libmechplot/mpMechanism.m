f72a0c9a1427a2dc33bfa4e8d4a3f638073323fc
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2013-11-01 15:48:26 -0700

    Options to save animations as image files.

--- libmechplot/mpMechanism.m
+++ libmechplot/mpMechanism.m
@@ -20,6 +20,12 @@ classdef mpMechanism  < handle
         
         % Plot options:
         keep_axis_limits = 0; % If set to 1, will leave the axis limits exactly as they were before starting to draw the structure
+        bg_color = []; % if set to an [R G B] color, will change the background of the current figure (gcf) 
+        
+        % Options for saving animations as sequences of images:
+        save_images = 0;  % Set to 1 to enable image saving (at the end of each call to .plot())
+        save_image_filenames = 'mechplot_%03d.png';
+        save_images_next_idx = 0; % The image index for "save_image_filenames" (automatically incremented)
     end
     
     properties(GetAccess=public, SetAccess=private)
@@ -51,10 +57,12 @@ classdef mpMechanism  < handle
             if (me.keep_axis_limits)
                 old_axis = axis;
             end
-            cla;
-            axis normal;            
-            hold on;
-            box on;
+            cla; axis normal;
+            hold on; box on;
+            
+            if (length(me.bg_color)==3)
+                set(gcf,'Color',me.bg_color);
+            end            
             
             % Sort by ascending "z_order" property:
             nObjs = length(me.objects);
@@ -86,8 +94,31 @@ classdef mpMechanism  < handle
             % Update window:            
             drawnow expose update;
             
+            % Save images to disk?
+            if (me.save_images)
+                me.do_save_image();
+            end
+                
         end % end of plot
         %% End of render
+        
+        % Saves the current contents of gcf() as an image, according to 
+        % the properties "save_image_filenames" and "save_images_next_idx"
+        function do_save_image(me)
+            I = getframe(gcf);
+            
+            sFil = sprintf(me.save_image_filenames,me.save_images_next_idx);            
+            [~,~,ext] = fileparts(sFil);
+            if (strcmpi(ext,'.gif'))
+                % Convert to 256 color image:
+                [imind,cm] = rgb2ind(frame2im(I),256);
+                imwrite(imind,cm,sFil,'gif');
+            else
+                % Save RGB image:
+                imwrite(I.cdata, sFil);
+            end
+            me.save_images_next_idx=me.save_images_next_idx+1;
+        end
         
         % Reset the cached structure dimension, used to automatically size
         % many elements when drawing.
